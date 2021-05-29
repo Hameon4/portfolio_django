58ccb13a74d5c68e9f100b9b4feba922054008ac
Hameon4 <61793446+Hameon4@users.noreply.github.com>
2021-05-29 15:09:33 +0400

    update

--- portfolio/website/forms.py
+++ portfolio/website/forms.py
@@ -8,7 +8,7 @@ class PostForm(forms.ModelForm):
         fields = ('title', 'title_tag', 'author', 'body')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Title'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
