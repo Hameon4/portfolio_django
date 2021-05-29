9ee2092c6668aba68b5f567c9d0b8ce1bcc65baa
Hameon4 <61793446+Hameon4@users.noreply.github.com>
2021-05-29 15:16:57 +0400

    update

--- portfolio/website/urls.py
+++ portfolio/website/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import BlogView, ArticleDetailView, AddPostView
+from .views import BlogView, ArticleDetailView, AddPostView, UpdatePostView
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('blog', BlogView.as_view(), name='blog'),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
     path('blog/add_post/', AddPostView.as_view(), name='add_post'),
+    path('blog/article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
 ]
